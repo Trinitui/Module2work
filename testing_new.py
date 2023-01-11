ad42bb7f2be6330fff6508333b3d2e43ff015bf1
Trinitui <andrewmwhelan9@gmail.com>
2023-01-11 23:08:43 +0000

    changes from gcp

--- testing_new.py
+++ testing_new.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-URL= "https://marvel.fandom.com/wiki/Goblin_Glider"
+URL= "https://wikipedia.com"
 print("Trying: ",URL)
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
